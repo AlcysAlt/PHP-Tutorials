3c4cdec32c4078a16eb064097aef904143c2a367
AlcysAlt <92039797+AlcysAlt@users.noreply.github.com>
2021-12-02 20:07:04 +0000

    Update Loops.php

--- Loops.php
+++ Loops.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
 <?php
-for ($i = 1; $i < 11; $i++){
-    for($a = 1; $a < 11; $a++){
-        echo($i." x ".$a." = ".$i*$a."<br>");
+function compute_table(){
+    for ($i = 1; $i < 11; $i++){
+        for($a = 1; $a < 11; $a++){
+            echo($i." x ".$a." = ".$i*$a."<br>");
+        }
     }
 }
+compute_table();
 ?>
