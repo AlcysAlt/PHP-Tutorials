448eb57ad2a10a6241b1c62d7dc6d4c5e4b7d8d4
AlcysAlt <92039797+AlcysAlt@users.noreply.github.com>
2022-01-16 11:08:19 +0000

    Update String Length.php

--- String Length.php
+++ String Length.php
@@ -4,12 +4,12 @@
 <?php
 $forename = 'Mateusz';
 $surname = 'Klocek';
-$encrolment = '30021114';
+$enrolment = '30021114';
 if (strlen($forename) > 5){
     echo $forename." ".$surname;
 }
 else{
-    echo $encrolment;
+    echo $enrolment;
 }
 
 ?>
